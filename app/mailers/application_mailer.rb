fcdb7f6733aa013c5c9c2dd8279b3c4f71e35db7
irene <irene.giberna@soluzionipa.it>
2020-07-01 13:59:21 +0200

    aggiunto nome certificato su metodi invio mail e mail errore certificato

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,8 +2,9 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'noreply@soluzionipa.it'
   layout 'mailer'
 
-  def cert_req_sent(email, nome)
+  def cert_req_sent(email, nome, certificato)
     @nome = nome
+    @certificato = certificato
     if !email.blank?
       mail(to: email, subject: 'Invio richiesta certificato')
     else
@@ -11,8 +12,9 @@ class ApplicationMailer < ActionMailer::Base
     end
   end
 
-  def cert_received_pay(email, nome)
+  def cert_received_pay(email, nome, certificato)
     @nome = nome
+    @certificato = certificato
     if !email.blank?
       mail(to: email, subject: 'Il tuo certificato è pronto')
     else
@@ -20,10 +22,21 @@ class ApplicationMailer < ActionMailer::Base
     end
   end
 
-  def cert_received_download(email, nome)
+  def cert_received_download(email, nome, certificato)
     @nome = nome
+    @certificato = certificato
     if !email.blank?
       mail(to: email, subject: 'Il tuo certificato è pronto')
+    else
+      logger.error "can't send email to blank email address"
+    end
+  end
+
+  def cert_failed(email, nome, certificato)
+    @nome = nome
+    @certificato = certificato
+    if !email.blank?
+      mail(to: email, subject: 'Errore richiesta certificato')
     else
       logger.error "can't send email to blank email address"
     end
